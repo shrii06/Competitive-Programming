07d7a0dd1ca4285867382f273b6ea79c11258d88
Shrinath D <57283832+shrii06@users.noreply.github.com>
2020-09-02 00:00:57 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned ll
+#define all(x) (x).begin(), (x).end()
+#define F first
+#define S second
+#define IOS std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+using namespace std;
+
+// DEBUG TEMPLATE
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << '\'' << x << '\'';}
+void __print(const char *x) {cout << '\"' << x << '\"';}
+void __print(const string &x) {cout << '\"' << x << '\"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+
+void OFFLINE(){
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+        #define debug(x...) cout << "[" << #x << "] = ["; _print(x)
+        #else
+            #define debug(x...)
+    #endif
+}
+
+const int MOD = 1e9 + 7;
+int main(){
+
+    IOS;
+    OFFLINE();
+    //check for corner test cases
+
+
+
+    //check for corner test cases
+    return 0;
+}
